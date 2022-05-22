5df33437680dc18963f07fa0e7a12211663ebce1
Van kocko <van.kocko@gmail.com>
2022-05-22 22:34:49 +0200

    install React

--- React/RappelJs/refreshArrayFunction.js
+++ React/RappelJs/refreshArrayFunction.js
@@ -50,9 +50,105 @@ console.log(values)
 console.log(['1','2','3'].map(Number))
 
 /**
- * 
+ *    Find function 
  */
 
 
+const arra1 = [5,12,8,130,44]
+
+const found = arra1.find(element  => element > 10)
+
+console.log(found)
 
 
+//   Find an object in an array by one of its properties
+
+const inventory = [
+    {name: 'apples', quantity: 2},
+    {name: 'bananas', quantity: 0},
+    {name: 'cherries', quantity: 5}
+  ];
+
+console.log(inventory.find(( fruit )=>{ return fruit.name === 'cherries'}))
+
+
+
+
+
+
+/**
+ *  Filter Function 
+ */
+
+
+ const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+
+ const result = words.filter(word => word.length > 6);
+ 
+ console.log(result);
+
+
+
+ /**
+  *   Reduce Function  : est un accumlateur 
+  * 
+  * La méthode reduce() applique une fonction qui est un « accumulateur » 
+  * et qui traite chaque valeur d'une liste (de la gauche vers la droite) 
+  * afin de la réduire à une seule valeur.
+  */
+
+  const array1 = [1, 2, 3, 4];
+
+  // 0 + 1 + 2 + 3 + 4
+  const initialValue = 0;
+  const sumWithInitial = array1.reduce(
+    (previousValue, currentValue) => previousValue + currentValue,
+    initialValue
+  );
+  
+  console.log(sumWithInitial);
+
+
+
+
+  /**
+   *    Concat function 
+   */
+
+
+
+   const concatarray1 = ['a', 'b', 'c'];
+   const concatarray2 = ['d', 'e', 'f'];
+   const concatarray3 = concatarray1.concat(concatarray2);
+   
+   console.log(concatarray3);
+
+
+   /**
+    *  Slice function 
+    */
+
+
+    const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+
+    console.log(animals.slice(2));
+
+    console.log(animals.slice(2, 4));
+
+
+/**
+ * splice function 
+ */
+
+
+ const months = ['Jan', 'March', 'April', 'June'];
+ months.splice(1, 0, 'Feb');
+ // inserts at index 1
+ console.log(months);
+ // expected output: Array ["Jan", "Feb", "March", "April", "June"]
+ 
+ months.splice(4, 1, 'May');
+ // replaces 1 element at index 4
+ console.log(months);
+ // expected output: Array ["Jan", "Feb", "March", "April", "May"]
+ 
