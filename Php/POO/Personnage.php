3c90946fae910c32ca1725c88b1396e6086043bd
Van kocko <van.kocko@gmail.com>
2022-06-17 08:58:25 +0200

    poo add

--- Php/POO/Personnage.php
+++ Php/POO/Personnage.php
@@ -3,8 +3,38 @@
 class Personnage {
     public $vie = 80;
     public $atk = 20;
-    public function regenerer(){
-        $this->vie = 100; 
+    public $nom;
+
+
+    /**
+     *  constructeur 
+     */
+    public function __construct($nom){
+       $this->nom = $nom;
+    }
+    
+    public function regenerer($vie = null){
+        if(is_null($vie)){
+            $this->vie = 100; 
+        }else{
+            $this->vie += $vie;
+        }
+    }
+    public function mort($personnage){
+        if($personnage->vie == 0){
+            echo "Le personnage du nom de : " .  $personnage->nom ." "." est mort";
+        }else{
+            echo "Le personnage du nom de : " .  $personnage->nom ." "."est vivant";
+
+        }
+           
+    }
+
+    public function attaque($cible){
+        $cible->vie = 20;
+      echo "<pre>";
+        print_r($cible);
+      echo "</pre>";
     }
 } 
 
--- Php/POO/index.php
+++ Php/POO/index.php
@@ -4,20 +4,29 @@
 require 'Personnage.php';
 
 
-
-$merlin = new Personnage();
+/**
+ *   Instance de la class 
+ */
+$merlin = new Personnage("Merlin");
+/**
+ *   Appel de methode d'instance d'elements 
+ */
 $merlin->regenerer();
 
-$harry = new Personnage();
+
+$harry = new Personnage( "Harry");
+
+$merlin->attaque($harry);
 
 
-echo "<pre>";
+
+/* echo "<pre>";
   print_r($harry);
 echo "</pre>";
 
 echo "<pre>";
   print_r($merlin);
-echo "</pre>";
+echo "</pre>"; */
 
 
 ?>
